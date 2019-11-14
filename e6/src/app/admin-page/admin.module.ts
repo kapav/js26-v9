56b340b95a792c8758660b01533c0881070a9d36
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-11-14 12:08:51 +0400

    Added sixth example affected guard mechanism in common and implementation of the 'canActivate' method.

--- e6/src/app/admin-page/admin.module.ts
+++ e6/src/app/admin-page/admin.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {AdminRoutingModule} from './admin-routing.module'
 
+import { AdminPageComponent } from './admin-page.component';
+import {AdminPhraseComponent} from '../shared/components/admin-phrase/admin-phrase.component'
+import {AdminUserComponent} from '../shared/components/admin-user/admin-user.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminPageComponent,
+    AdminPhraseComponent,
+    AdminUserComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
